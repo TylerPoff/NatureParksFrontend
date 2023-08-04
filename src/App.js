7453a422eafb625c8503094b920aff1b4d527eb9
TylerPoff <Tyler.Poff1900@gmail.com>
2023-08-04 12:47:12 -0400

    Setup navbar for page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,33 @@ import { Navbar, Nav } from "react-bootstrap";
 import './App.css';
 
 function App() {
-
+  return (
+    <div className="App">
+      <Navbar className="nav" expand="lg" sticky="top" variant="dark">
+        <Container className="container-fluid">
+          <div className="nav-link-group">
+            <Nav.Link as={Link} to="/parks" className="nav-link">
+              Parks
+            </Nav.Link>
+            <Nav.Link as={Link} to="/trips" className="nav-link">
+              My Trips
+            </Nav.Link>
+          </div>
+          <Navbar.Brand href="/">
+            <img src="" alt="park logo" className="parks-logo"/>
+          </Navbar.Brand>
+          <div className="nav-link-group">
+            <Nav.Link as={Link} to="/lists" className="nav-link">
+              Bucketlist
+            </Nav.Link>
+            <Nav.Link as={Link} to="/login" className="nav-link">
+              Login
+            </Nav.Link>
+          </div>
+        </Container>
+      </Navbar>
+    </div>
+  )
 }
 
 export default App;
