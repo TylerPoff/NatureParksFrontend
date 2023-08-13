14cb1fe5f058e6aa9a7f1ef5a60574723ae5544a
TylerPoff <Tyler.Poff1900@gmail.com>
2023-08-13 15:14:33 -0400

    Add basics of trips page design

--- src/components/Trips.js
+++ src/components/Trips.js
@@ -14,13 +14,55 @@ import "./ParksList";
 const Trips = ({
     user
 }) => {
+
+    const [tripTitle, setTripTitle] = useState('');
+    const [location, setLocation] = useState('');
+    const [favoriteMemory, setFavoriteMemory] = useState('');
+    const [summary, setSummary] = useState('');
     
     return (
         <div className="App">
             <Container className="main-container">
                 {user ? (
-                    <div>
-
+                    <div className="userGreet">
+                        <h1 className="nullText">
+                            Hello, {user.name} 
+                        </h1>
+                        <div className="logLink">
+                            <Link to="/trips">
+                                Add New Log
+                            </Link>
+                            <Link to="/trips/old">
+                                View Old Logs
+                            </Link>
+                        </div>
+                        <div className="textField">
+                            <input
+                                type="text"
+                                placeholder="Trip Title"
+                                value={tripTitle}
+                                onChange={(e) => setTripTitle(e.target.value)}
+                            />
+                            <input
+                                type="text"
+                                placeholder="Location"
+                                value={location}
+                                onChange={(e) => setLocation(e.target.value)}
+                            />
+                            <textarea
+                                placeholder="Summary"
+                                value={favoriteMemory}
+                                onChange={(e) => setFavoriteMemory(e.target.value)}
+                            />
+                            <textarea 
+                                placeholder="Favorite Memory"
+                                value={summary}
+                                onChange={(e) => setSummary(e.target.value)}
+                            />
+                            <Button className="submitBtn">
+                                Submit
+                            </Button>
+                        </div>
                     </div>
                 ) : (
                     <div className="nullCenter">
